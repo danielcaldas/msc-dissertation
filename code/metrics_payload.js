31f91131f8ff9cb70d2fc62bb9901b3b3cfc4e5b
danielcaldas <danielcaldas@sapo.pt>
2017-06-21 19:26:03 +0100

    Remove verbatins in Chapter 7. Add lstings instead

---
+++ code/metrics_payload.js
@@ -0,0 +1,16 @@
+{
+    directed: {boolean},
+    graph: {name: "Graph's Name"}
+    links: [{source: 0, target:1}, ...],
+    nodes: [{id: 0}, ...],
+    metrics: [
+        'averageClustering',
+        'betweennessCentrality',
+        'closenessCentrality',
+        'clusteringCoefficient'
+        'degree',
+        'degreeCentrality',
+        'eigenvectorCentrality',
+        'pageRank'
+    ]
+}
---
+++ code/metrics_response.js
@@ -0,0 +1,12 @@
+{
+    "0": {
+        "betweennessCentrality": 0.006629585250616832,
+        "closenessCentrality": 0.29365513766170576,
+        "degreeCentrality": 0.026819923371647507,
+        "eigenvectorCentrality": 4.889334831194343e-05
+    },
+    "1": {...},
+    "global": {
+        "averageClustering": 0.0434353
+    }
+}
