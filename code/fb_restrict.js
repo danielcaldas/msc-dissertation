8c69ad0875ee70e5e8d2461f4f56b5b2e1e76a88
Daniel Caldas <danielcaldas@sapo.pt>
2017-09-22 23:35:56 +0100

    Chapter 7 specific fixes

--- code/fb_restrict.js
+++ code/fb_restrict.js
@@ -4,9 +4,9 @@ var facebookThreshold = {
     MAX_FB_REACTION_VALUE: 1000,
     MAX_FB_POST_COMMENTS: 1000,
     MAX_FB_POST_SHARED: 1000,
-    // There is a 30% changes of no reactions for a given post
+    // There is a 30% chance of no reactions for a given post
     NO_REACTION_PROB: 30,
-    // There is a 60% changes that a given post only receives likes (as reaction)
+    // There is a 60% chance that a given post only receives likes (as reaction)
     ONLY_LIKES_REACTION_PROB: 60,
     // There is 40% chance that some post is not commented
     NO_COMMENTS_PROB: 40,
